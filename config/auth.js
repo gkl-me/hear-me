fa124b53db63a57ba23d775d48dfa82e7a4f5b0b
Gokul G R <gklgrme@gmail.com>
2024-07-12 10:40:57 +0530

    google auth

--- config/auth.js
+++ config/auth.js
@@ -23,7 +23,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/user/auth/google/redirect"
+    callbackURL: "https://hearme.life/user/auth/google/redirect"
   },async (accessToken, refreshToken, profile, done)=> {
 
     try {
